ef564fe7c021de886339be26a61e1b7425f8a68e
22veloso <lucasveloso2205@gmail.com>
2022-10-03 20:53:16 -0300

    ex

---
+++ apiFuncionarios/Data/ApiFuncionariosContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiFuncionarios.Models;
+
+namespace ApiFuncionarios.Data
+{
+    public class ApiFuncionariosContext : DbContext
+    {
+        public ApiFuncionariosContext (DbContextOptions<ApiFuncionariosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiFuncionarios.Models.Funcionario> Funcionario { get; set; } = default!;
+    }
+}
---
+++ apiFuncionarios/Models/Funcionario.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiFuncionarios.Models
+{
+    public class Funcionario
+    {
+        public int Id{ get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public double Salario { get; set; }
+    }
+}
